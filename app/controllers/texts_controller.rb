8267a0364e6b66c71f1ce6df9870e358cc7559b2
Asher King Abramson <a.kingabramson@gmail.com>
2013-11-01 10:59:51 -0700

    cleaned up

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,8 +1,11 @@
 class TextsController < ApplicationController
 
   def create
-    EmailFriend.send_email(params["Body"], params["From"], 
-                           ENV["Friend_Email"]).deliver
+    EmailFriend.send_email(
+      params["Body"], 
+      params["From"], 
+      ENV["Friend_Email"]
+    ).deliver
     
     # In the future, add a backend so users can send
     # to multiple friends.  Currently in trial mode, so
